1a1316310f1e8cb5ec2fc0be8c6ad04ea28e0236
Smalltalk1357 <alex.wan@icloud.com>
2025-10-01 18:15:02 +0100

    Added second part of instructions

--- LCO2Day06/LCO2Day06/Day06Task2.cs
+++ LCO2Day06/LCO2Day06/Day06Task2.cs
@@ -1,18 +1,25 @@
 namespace LCO2Day06;
 
+// Task 2:
 // Convert the word hello to its character codes (one integer for each code) and output them.
 // Do this for hello in Chinese as well - In your output message you may wish to use nihao as 你好 may not print to the console
 // Test this procedure by calling it from main
+
+// Task 3:
+// Convert the word hello to its character codes expressed as a byte array
+// You can use Encoding.Default.GetBytes() for this, then output the result
+// Remember a byte is nothing but an integer between 0-255 that uses one byte of memory
+// Do this for hello in Chinese as well
 
 public class Day06Task2
 {
     public static void Run()
     {
-        string helloCn = "你好";
         string helloEn = "Hello";
+        string helloCn = "你好";
         
-        Console.WriteLine($"Character codes of {helloCn} are {CharacterCodes(helloCn)}.");
         Console.WriteLine($"Character codes of {helloEn} are {CharacterCodes(helloEn)}.");
+        Console.WriteLine($"Character codes of {helloCn} are {CharacterCodes(helloCn)}.");
     }
 
     private static string CharacterCodes(string input = "hello")
@@ -20,7 +27,7 @@ public class Day06Task2
         string result = "";
         foreach (char character in input)
         {
-            result += (int)character; // returns the code of the character
+            result += (int)character; // returns the character code of the character
             result += " ";
         }
         return result;
