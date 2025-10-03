3af8250ef1469b5af864ec4325f1dfc465bd5e50
Smalltalk1357 <alex.wan@icloud.com>
2025-10-03 21:06:22 +0100

    Created Day06Task8.cs

---
+++ LCO2Day06/LCO2Day06/Day06Task8.cs
@@ -0,0 +1,12 @@
+namespace LCO2Day06;
+
+// Pig latin: move the first letter to the end of the word and add ay
+public class Day06Task8
+{
+    public static void Run()
+    {
+        
+    }
+    
+    
+}
