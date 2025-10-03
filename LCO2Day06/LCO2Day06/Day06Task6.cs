22e4d414cba7a29d48010fe4059bbf26e1769624
Smalltalk1357 <alex.wan@icloud.com>
2025-10-03 18:28:50 +0100

    Created Day06Task6.cs

---
+++ LCO2Day06/LCO2Day06/Day06Task6.cs
@@ -0,0 +1,21 @@
+namespace LCO2Day06;
+
+// Anagram: Given a word and a list of words return the number of words that are anagrams of the others
+public class Day06Task6
+{
+    public static void Run()
+    {
+        
+    }
+
+    public static bool IsAnagram(string word1, string word2)
+    {
+        List<char> word1Chars = word1.ToCharArray().ToList();
+        List<char> word2Chars = word2.ToCharArray().ToList();
+        
+        word1Chars.Sort();
+        word2Chars.Sort();
+        
+        return word1Chars.SequenceEqual(word2Chars);
+    }
+}
