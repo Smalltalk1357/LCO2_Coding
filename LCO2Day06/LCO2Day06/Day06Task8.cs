fb430544d5a0e904a4b6d87e5ccccac29e580427
Smalltalk1357 <alex.wan@icloud.com>
2025-10-04 15:38:00 +0100

    PigLatin now ignores punctuation

--- LCO2Day06/LCO2Day06/Day06Task8.cs
+++ LCO2Day06/LCO2Day06/Day06Task8.cs
@@ -5,24 +5,42 @@ public class Day06Task8
 {
     public static void Run()
     {
-        string input = "The cat sat on the mat.";
+        string input = "The cat \"\"\" sat on the mat.";
         Console.WriteLine(PigLatin(input));
     }
 
-    public static string PigLatin(string input)
+    private static string PigLatin(string input)
     {
         string[] words = input.Split(' ');
         string result = "";
         
         foreach (string word in words)
         {
-            string output = "";
-            
-            char firstChar = word[0];
-            string tempWord = word.Substring(1);
-            
-            output = tempWord + firstChar + "ay";
-            result += output + ' ';
+            // Needs to check for punctuation - ignore words composed of only punctuation
+            bool isPunctuation = false;
+            foreach (char character in word)
+            {
+                if (char.IsPunctuation(character))
+                {
+                    isPunctuation = true;
+                    break;
+                }
+            }
+
+            if (!isPunctuation)
+            {
+                string output = "";
+
+                char firstChar = word[0];
+                string tempWord = word.Substring(1);
+
+                output = tempWord + firstChar + "ay";
+                result += output + ' ';
+            }
+            else
+            {
+                result += word + ' ';
+            }
         }
         
         result = result.TrimEnd(' ');
