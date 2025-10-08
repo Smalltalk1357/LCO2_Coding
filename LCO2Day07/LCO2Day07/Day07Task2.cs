98815152f38827313617dccacc9064df1bfcc8c2
Smalltalk1357 <alex.wan@icloud.com>
2025-10-08 21:59:18 +0100

    Completed Q2

--- LCO2Day07/LCO2Day07/Day07Task2.cs
+++ LCO2Day07/LCO2Day07/Day07Task2.cs
@@ -2,26 +2,44 @@ namespace LCO2Day07;
 
 public class Day07Task2
 {
+    // For Q2, the station name is Battersea Power Station
+    
     public static void Run()
     {
         string filename = Utils.GetCurrentDirectory("stations.txt");
+        List<string> input = ParseFile(filename);
+        List<string> output = SearchForTerm("Station", input);
+        Console.WriteLine(output[0]);
     }
     
-    private static List<string> ParseFile()
+    private static List<string> ParseFile(string filename)
+    {
+        List<string> output = [];
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            output.Add(line);
+        }
+        return output;
+    }
+    private static string ParseTerm(string input, int index = 0)
+    {
+        string[] inputTerms = input.Split(",");
+        return inputTerms[index]; // In the case of stations.txt, the first term is the station name
+    }
+    
+    private static List<string> SearchForTerm(string term, List<string> input)
     {
         List<string> output = [];
         
-        while (true)
+        foreach (string line in input)
         {
-            string newLine = Console.ReadLine();
-            if (newLine == "")
+            if (line.ToLower().Contains(term.ToLower()))
             {
-                return output;
+                string returnValue = ParseTerm(line);
+                output.Add(returnValue);
             }
         }
-    }
-    private static void ParseTerm(string term, List<string> lines)
-    {
-        
+        return output;
     }
 }
