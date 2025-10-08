485f443750fabea1a404b54a8b3fcf79803cb70d
Smalltalk1357 <alex.wan@icloud.com>
2025-10-08 20:37:27 +0100

    The file now will be in the same location as the root

--- LCO2Day07/LCO2Day07/Day07Task1.cs
+++ LCO2Day07/LCO2Day07/Day07Task1.cs
@@ -4,9 +4,13 @@ public class Day07Task1
 {
     public static void Run()
     {
+        Console.Clear();
+        Console.WriteLine("--- EDITING --- (return empty line to exit");
         List<string> writeToFile = ParseFile();
-        CreateFile(writeToFile);
-        Console.WriteLine("File Created");
+        Console.WriteLine("--- END OF FILE ---");
+        string outputPath = GetCurrentDirectory("sentences.txt");
+        WriteToFile(writeToFile, outputPath);
+        Console.WriteLine("--- FILE CREATED ---");
     }
 
     public static List<string> ParseFile()
@@ -23,8 +27,17 @@ public class Day07Task1
             output.Add(newLine);
         }
     }
+
+    private static string GetCurrentDirectory(string filename = "") 
+    {
+        // I used some help to find the file path - suggested by AI
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string projectRoot = Path.GetFullPath(Path.Combine(currentDirectory, "../../../"));
+        string outputPath = Path.Combine(projectRoot, filename);
+        return outputPath;
+    }
     
-    public static void CreateFile(List<string> input, string filename = "sentences.txt")
+    private static void WriteToFile(List<string> input, string filename)
     {
         File.WriteAllLines(filename, input);
     }
