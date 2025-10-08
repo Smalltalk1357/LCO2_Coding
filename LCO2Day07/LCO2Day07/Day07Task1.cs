bbbce774d03a7ffee89360ed862ed6f91c6f5d68
Smalltalk1357 <alex.wan@icloud.com>
2025-10-08 18:49:30 +0100

    Finished Q1

--- LCO2Day07/LCO2Day07/Day07Task1.cs
+++ LCO2Day07/LCO2Day07/Day07Task1.cs
@@ -2,5 +2,30 @@ namespace LCO2Day07;
 
 public class Day07Task1
 {
+    public static void Run()
+    {
+        List<string> writeToFile = ParseFile();
+        CreateFile(writeToFile);
+        Console.WriteLine("File Created");
+    }
+
+    public static List<string> ParseFile()
+    {
+        List<string> output = [];
+        
+        while (true)
+        {
+            string newLine = Console.ReadLine();
+            if (newLine == "")
+            {
+                return output;
+            }
+            output.Add(newLine);
+        }
+    }
     
+    public static void CreateFile(List<string> input, string filename = "sentences.txt")
+    {
+        File.WriteAllLines(filename, input);
+    }
 }
