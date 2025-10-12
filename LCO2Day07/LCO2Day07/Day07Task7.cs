007886a6a82c6001aee4a0959a609c4fea0b1498
Smalltalk1357 <alex.wan@icloud.com>
2025-10-12 23:07:24 +0100

    New commit (WIP)

--- LCO2Day07/LCO2Day07/Day07Task7.cs
+++ LCO2Day07/LCO2Day07/Day07Task7.cs
@@ -7,8 +7,11 @@ public class Day07Task7
         
     }
 
-    public static void TestFile()
+    public static List<char> DisplayFile(string filename, int startRange = 0, int endRange = 10)
     {
+        List<string> lines = Utils.FileToList(filename);
+
+        string textFile = string.Join(' ', lines);
         
     }
 }
